539a63a0a0b8a2405dfcd7590937067e90162c7f
Rafael Dias <rafael.ddsociety@gmail.com>
2023-10-26 14:16:05 -0300

    feature: instalacao de font padrao Inter, ajustes no navbar e hero

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,42 +1,42 @@
-import { ThemeBtn } from "@/pages/_app";
-import { Box, Container, Typography, useTheme } from "@mui/material";
+import { ThemeBtn } from '@/pages/_app'
+import { Box, Container, Typography, useTheme } from '@mui/material'
 
 export default function Hero() {
-  const theme = useTheme();
+  const theme = useTheme()
 
   return (
-    <Box bgcolor="background.default">
-      <Container sx={{ minHeight: "40vh" }}>
+    <Box bgcolor='background.default'>
+      <Container sx={{ height: '580px' }}>
         <Box
-          display={["flex"]}
-          flexDirection={["column", "column", "row"]}
+          display={['flex']}
+          flexDirection={['column', 'column', 'row']}
           gap={[2, 2, 3, 4]}
-          px={"24px"}
+          px={'24px'}
           pt={8}
           pb={[0, 0, 1, 2]}
-          height="100%"
+          height='100%'
         >
           <Box
-            display={"flex"}
-            flexDirection={["column", "column"]}
-            justifyContent={"center"}
+            display={'flex'}
+            flexDirection={['column', 'column']}
+            justifyContent={'center'}
             gap={[2, 2, 3, 4]}
-            width={["100%", "100%", "50%", "40%"]}
+            width={['100%', '100%', '50%', '40%']}
           >
             <Typography
-              color="text.primary"
-              fontSize={["30px", "40px", "48px"]}
-              fontWeight={"bold"}
-              variant="h1"
+              color='text.primary'
+              fontSize={['30px', '40px', '48px']}
+              fontWeight={'bold'}
+              variant='h1'
             >
               Saiba tudo em um só lugar.
             </Typography>
 
             <Typography
-              color="text.primary"
-              fontSize={["14px", "15px", "16px"]}
-              fontWeight={"bold"}
-              variant="h1"
+              color='text.primary'
+              fontSize={['14px', '15px', '16px']}
+              fontWeight={'bold'}
+              variant='h1'
             >
               Personagens, localizações, episódios e muito mais.
             </Typography>
@@ -46,39 +46,21 @@ export default function Hero() {
 
           <Box
             sx={{
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-              backgroundPosition: "center",
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'contain',
+              backgroundPosition: 'center',
               backgroundImage: `url(${
-                theme.palette.mode === "dark"
-                  ? "/homeimages/heroImage.png"
-                  : "/homeimages/herolightimage.png"
+                theme.palette.mode === 'dark'
+                  ? '/homeimages/heroImage.png'
+                  : '/homeimages/herolightimage.png'
               })`,
             }}
-            ml="auto"
-            width={["100%", "100%", "50%", "50%"]}
-            minHeight={"30vh"}
-          >
-            {/*<Image
-              width={300}
-              height={300}
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "contain",
-                objectPosition: "bottom",
-                maxHeight: "400px",
-              }}
-              alt="heroimage"
-              src={
-                theme.palette.mode === "dark"
-                  ? "/homeimages/heroImage.png"
-                  : "/homeimages/herolightimage.png"
-              }
-            />*/}
-          </Box>
+            ml='auto'
+            width={['100%', '100%', '50%', '50%']}
+            minHeight={'30vh'}
+          ></Box>
         </Box>
       </Container>
     </Box>
-  );
+  )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import client from "@/lib/apolloClient";
-import "@/styles/globals.css";
-import { ApolloProvider } from "@apollo/client";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
+import client from '@/lib/apolloClient'
+import '@/styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
 import {
   Box,
   Button,
@@ -10,50 +10,59 @@ import {
   IconButton,
   PaletteMode,
   useTheme,
-} from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import type { AppProps } from "next/app";
-import * as React from "react";
+} from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import type { AppProps } from 'next/app'
+import * as React from 'react'
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [mode, setMode] = React.useState<"light" | "dark">("light");
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light')
   const colorMode = React.useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
       },
     }),
-    []
-  );
+    [],
+  )
 
   const getDesignTokens = (mode: PaletteMode) => ({
     palette: {
       mode,
-      ...(mode === "light"
+      ...(mode === 'light'
         ? {
             // palette values for light mode
             background: {
-              default: "fafafa",
+              default: 'fafafa',
             },
             text: {
-              primary: "#000000",
+              primary: '#000000',
             },
           }
         : {
             // palette values for dark mode
             background: {
-              default: "#000000",
+              default: '#000000',
             },
             text: {
-              primary: "#fff",
+              primary: '#fff',
             },
           }),
     },
-  });
+  })
 
-  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        ...getDesignTokens(mode),
+        typography: {
+          fontFamily: ['Inter', 'Roboto', 'sans-serif'].join(','),
+        },
+      }),
+    [mode],
+  )
 
   return (
     <ApolloProvider client={client}>
@@ -64,32 +73,32 @@ export default function App({ Component, pageProps }: AppProps) {
         </ThemeProvider>
       </ColorModeContext.Provider>
     </ApolloProvider>
-  );
+  )
 }
 
 export function ThemeBtn() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
+  const theme = useTheme()
+  const colorMode = React.useContext(ColorModeContext)
   return (
     <>
       <Button
         onClick={colorMode.toggleColorMode}
-        variant="contained"
+        variant='contained'
         startIcon={
-          theme.palette.mode === "dark" ? (
+          theme.palette.mode === 'dark' ? (
             <Brightness7Icon />
           ) : (
             <Brightness4Icon />
           )
         }
         sx={{
-          width: "180px",
-          backgroundColor: "#11B0C8",
-          fontWeight: "bold",
+          width: '180px',
+          backgroundColor: '#11B0C8',
+          fontWeight: 'bold',
         }}
       >
         Mudar tema
       </Button>
     </>
-  );
+  )
 }
