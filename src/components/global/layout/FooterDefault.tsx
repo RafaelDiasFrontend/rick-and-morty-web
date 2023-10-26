ac33a8c768f883fe4214c5463746f71483965072
Rafael Dias <rafael.ddsociety@gmail.com>
2023-10-26 11:11:52 -0300

    footer

--- src/components/global/layout/FooterDefault.tsx
+++ src/components/global/layout/FooterDefault.tsx
@@ -1,24 +1,40 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 import LinkTo from '../LinkTo'
 
 export default function FooterDefault() {
   return (
     <>
-      <Box
-        sx={{
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          m: 3,
-        }}
-        display='flex'
-      >
-        <div>
-          <img src='/svgs/ricky.svg' alt='ricky and morty' />
-        </div>
-        <LinkTo href='#hero-img'>
-          <div>Voltar ao topo</div>
-        </LinkTo>
+      <Box sx={{ m: 3 }}>
+        <Box
+          sx={{
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+          }}
+          display='flex'
+        >
+          <div>
+            <img src='/svgs/ricky.svg' alt='ricky and morty' />
+          </div>
+          <LinkTo href='#hero-img'>
+            <div>Voltar ao topo</div>
+          </LinkTo>
+        </Box>
+        <Box
+          sx={{
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+          display='flex'
+        >
+          <Typography component='h4' variant='body1' color='textPrimary'>
+            ©2023
+          </Typography>
+          <Typography component='h4' variant='body1' color='textPrimary'>
+            Todos os direitos reservados
+          </Typography>
+        </Box>
       </Box>
     </>
   )
